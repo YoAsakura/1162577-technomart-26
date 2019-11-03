29fe82f1cb21a3981c21c2f2a8781b80cf49a9de
Roman Murunov <54361554+YoAsakura@users.noreply.github.com>
2019-11-04 00:18:57 +0300

    Слайдер и ласт коррект

---
+++ js/buypopup.js
@@ -0,0 +1,29 @@
+var buylink = document.querySelectorAll(".button-buy");
+var buypopup = document.querySelector(".modal-buy");
+var close = buypopup.querySelector(".close");
+var buttoncontinue = buypopup.querySelector(".button-continue");
+
+close.addEventListener("click", function (event) {
+  event.preventDefault();
+  buypopup.classList.remove("content-show")
+});
+
+buttoncontinue.addEventListener("click", function (event) {
+  event.preventDefault();
+  buypopup.classList.remove("content-show")
+});
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+    if (buypopup.classList.contains("content-show")) {
+      buypopup.classList.remove("content-show")
+    }
+  }
+});
+
+buylink.forEach(element => {
+  element.addEventListener("click", function (event) {
+    event.preventDefault();
+    buypopup.classList.add("content-show");
+})
+});
